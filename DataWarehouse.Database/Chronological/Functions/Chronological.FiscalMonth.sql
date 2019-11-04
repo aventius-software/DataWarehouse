1c939af637d617b3d10e707b2e4f095ccdcfbd1e
Ben <noreply@aventius.co.uk>
2019-11-04 08:53:18 +0000

    fixed a bug and added fiscal year month start date with default parameter value

--- DataWarehouse.Database/Chronological/Functions/Chronological.FiscalMonth.sql
+++ DataWarehouse.Database/Chronological/Functions/Chronological.FiscalMonth.sql
@@ -1,9 +1,14 @@
 ﻿
 CREATE FUNCTION [Chronological].[FiscalMonth] (
-	@date DATE	
+	@date DATE
+	,@fiscalYearStartMonth TINYINT = 4
 )
 RETURNS TINYINT
 AS
 BEGIN
-	RETURN MONTH(DATEADD(MONTH, -3, @date))
+	-- return invalid value if the start month is not valid
+	IF NOT @fiscalYearStartMonth BETWEEN 1 AND 12 RETURN -1
+
+	-- otherwise return correct value
+	RETURN MONTH(DATEADD(MONTH, -1 * (@fiscalYearStartMonth - 1), @date))
 END
